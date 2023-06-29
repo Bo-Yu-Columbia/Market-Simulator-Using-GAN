28362f40efe6f3d5db23beb0ca37f52294f6fd64
Bo-Yu-Columbia <by2345@columbia.edu>
2023-06-29 01:30:26 -0400

    test new data loader

--- hyperparameters.py
+++ hyperparameters.py
@@ -116,6 +116,12 @@ SIGCWGAN_CONFIGS = dict(
         sig_config_future=SignatureConfig(depth=2,
                                           augmentations=tuple([Scale(0.2), Cumsum(), AddLags(m=2), LeadLag()])),
     ),
+    EIB_5yr=SigCWGANConfig(
+        mc_size=1000,
+        sig_config_past=SignatureConfig(depth=2, augmentations=tuple([Scale(0.2), Cumsum(), AddLags(m=2), LeadLag()])),
+        sig_config_future=SignatureConfig(depth=2,
+                                          augmentations=tuple([Scale(0.2), Cumsum(), AddLags(m=2), LeadLag()])),
+    ),
     EXCHANGE_JPYUSD=SigCWGANConfig(
         mc_size=1000,
         sig_config_past=SignatureConfig(depth=2, augmentations=tuple([Scale(0.2), Cumsum(), AddLags(m=2), LeadLag()])),
