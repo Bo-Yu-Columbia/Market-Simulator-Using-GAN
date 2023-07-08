a6980ec5591e4c9759192b28ee5f772a259bb54f
Bo-Yu-Columbia <by2345@columbia.edu>
2023-07-08 12:34:24 -0400

    data

--- train.py
+++ train.py
@@ -165,7 +165,7 @@ def get_dataset_configuration(dataset):
         generator = [('a', dict())]
     elif dataset == 'EIB':
         generator = (('_'.join(duration), dict(durations=duration))
-                     for duration in [('1yr',), ('1yr', '5yr')])
+                     for duration in [('1yr',)])
                      # for duration in [('1yr',), ('1yr', '5yr'), ('1yr', '5yr', '10yr')])
     else:
         # if the dataset is not recognized, it raises an exception
