fb136fc16bda7761e5e24b29ec712dc49fc8ff90
Tunahan Gumuskaya <tg2885@columbia.edu>
2023-07-08 13:02:44 -0400

    1year_5year_10_20_30year

--- hyperparameters.py
+++ hyperparameters.py
@@ -122,6 +122,25 @@ SIGCWGAN_CONFIGS = dict(
         sig_config_future=SignatureConfig(depth=2,
                                           augmentations=tuple([Scale(0.2), Cumsum(), AddLags(m=2), LeadLag()])),
     ),
+    EIB_1yr_5yr_10yr=SigCWGANConfig(
+        mc_size=1000,
+        sig_config_past=SignatureConfig(depth=2, augmentations=tuple([Scale(0.2), Cumsum(), AddLags(m=2), LeadLag()])),
+        sig_config_future=SignatureConfig(depth=2,
+                                          augmentations=tuple([Scale(0.2), Cumsum(), AddLags(m=2), LeadLag()])),
+    ),
+    EIB_1yr_5yr_10yr_20yr=SigCWGANConfig(
+        mc_size=1000,
+        sig_config_past=SignatureConfig(depth=2, augmentations=tuple([Scale(0.2), Cumsum(), AddLags(m=2), LeadLag()])),
+        sig_config_future=SignatureConfig(depth=2,
+                                          augmentations=tuple([Scale(0.2), Cumsum(), AddLags(m=2), LeadLag()])),
+    ),
+        EIB_1yr_5yr_10y_20yr_30yr=SigCWGANConfig(
+        mc_size=1000,
+        sig_config_past=SignatureConfig(depth=2, augmentations=tuple([Scale(0.2), Cumsum(), AddLags(m=2), LeadLag()])),
+        sig_config_future=SignatureConfig(depth=2,
+                                          augmentations=tuple([Scale(0.2), Cumsum(), AddLags(m=2), LeadLag()])),
+    ),
+
     EXCHANGE_JPYUSD=SigCWGANConfig(
         mc_size=1000,
         sig_config_past=SignatureConfig(depth=2, augmentations=tuple([Scale(0.2), Cumsum(), AddLags(m=2), LeadLag()])),
