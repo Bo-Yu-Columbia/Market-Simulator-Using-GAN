e5c97127a71bccdd3bcf4d71572bed1562af6815
Tunahan Gumuskaya <tg2885@columbia.edu>
2023-07-29 10:15:37 -0400

    all the year combinations added

--- hyperparameters.py
+++ hyperparameters.py
@@ -263,12 +263,6 @@ SIGCWGAN_CONFIGS = dict(
         sig_config_future=SignatureConfig(depth=2,
                                           augmentations=tuple([Scale(0.2), Cumsum(), AddLags(m=2), LeadLag()])),
     ),
-    EIB_30yr=SigCWGANConfig(
-        mc_size=1000,
-        sig_config_past=SignatureConfig(depth=2, augmentations=tuple([Scale(0.2), Cumsum(), AddLags(m=2), LeadLag()])),
-        sig_config_future=SignatureConfig(depth=2,
-                                          augmentations=tuple([Scale(0.2), Cumsum(), AddLags(m=2), LeadLag()])),
-    ),
     EIB_1yr_5yr_10yr_30yr=SigCWGANConfig(
         mc_size=1000,
         sig_config_past=SignatureConfig(depth=2, augmentations=tuple([Scale(0.2), Cumsum(), AddLags(m=2), LeadLag()])),
